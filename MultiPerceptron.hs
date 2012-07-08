92b900e0588060e1f23a34b78f1b2fc161a94243
matthew <matthew@mattdesktop.(none)>
2012-07-07 23:06:02 -0700

    sanity check for functions

--- MultiPerceptron.hs
+++ MultiPerceptron.hs
@@ -3,6 +3,7 @@ module MultiPerceptron where
 
 --import qualified Data.ByteString.Lazy.Char8 as L
 import qualified Data.Vector.Unboxed as V
+-- import Test.QuickCheck
 
 class SupervisedLearner l  where
     -- l is a learner function
@@ -23,7 +24,7 @@ type Weights = V.Vector Double
 
 
 
--- the input that we need
+-- internally used in the idea of a perceptron being a comp machine
 calcWeights :: Input -> Weights -> Double
 calcWeights i w  =  V.sum $ V.zipWith (*) (inInput i) w 
 
@@ -31,8 +32,14 @@ sigmoid :: Double -> Double
 sigmoid x = 1.0 / (1 + exp (-x))
 
 kClassOutput :: V.Vector Double -> Output Double
-kClassOutput  i =  Kclass $ V.map  (\x-> exp x / total) i 
-        where total =  V.foldr' (\a b -> ((exp a) + b)) 0 i
+kClassOutput i 
+    | V.null i = Kclass $ V.singleton 1.0
+    | otherwise  =  Kclass $ V.map (\x-> exp x / total) i 
+        where total =  V.foldr' (\g h -> exp g + h) 0 i
+
+-- simple sanity check
+prop_sumToOne b =  (num  < 1.001) && (num > 0.99) 
+    where num = V.sum . outClass $ kClassOutput $ V.fromList b 
 
 
 -- testing stuff
@@ -40,9 +47,7 @@ b = V.fromList [1.0,2.0,3.0,4.0,5.0]
 o = V.fromList [1.3,2.2,3.2,4.4,5.4]
 a =  Supervised {inInput=b,inOutput=o}
 
-g = (V.sum . outClass $ kClassOutput b ) == 1.0
 
---threshold = undefined
 
 {-
 parse :: L.ByteString -> V.Vector Int
